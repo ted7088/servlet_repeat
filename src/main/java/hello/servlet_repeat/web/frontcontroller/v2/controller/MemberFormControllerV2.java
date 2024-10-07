43c46c9b6b723e1dd4756031101228366689776c
master <ted3131@diquest.com>
2024-10-07 13:49:40 +0900

    commit

---
+++ src/main/java/hello/servlet_repeat/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -0,0 +1,16 @@
+package hello.servlet_repeat.web.frontcontroller.v2.controller;
+
+import hello.servlet_repeat.web.frontcontroller.MyView;
+import hello.servlet_repeat.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV2 implements ControllerV2 {
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        return new MyView("/WEB-INF/views/new-form.jsp");
+    }
+}
---
+++ src/main/java/hello/servlet_repeat/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -0,0 +1,27 @@
+package hello.servlet_repeat.web.frontcontroller.v2.controller;
+
+import hello.servlet_repeat.domain.member.Member;
+import hello.servlet_repeat.domain.member.MemberRepository;
+import hello.servlet_repeat.web.frontcontroller.MyView;
+import hello.servlet_repeat.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV2 implements ControllerV2 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        request.setAttribute("member", member);
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
+    }
+
+
+}
