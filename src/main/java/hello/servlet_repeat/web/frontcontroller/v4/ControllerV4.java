f428d3edf5dbce9728aa81188f8f233ed0e256d2
master <ted3131@diquest.com>
2024-10-30 14:44:05 +0900

    commit

--- src/main/java/hello/servlet_repeat/web/frontcontroller/v4/ControllerV4.java
+++ src/main/java/hello/servlet_repeat/web/frontcontroller/v4/ControllerV4.java
@@ -3,10 +3,5 @@ package hello.servlet_repeat.web.frontcontroller.v4;
 import java.util.Map;
 
 public interface ControllerV4 {
-    /**
-     * @param paramMap
-     * @param model
-     * @return viewName
-     */
     String process(Map<String, String> paramMap, Map<String, Object> model);
 }
---
+++ src/main/java/hello/servlet_repeat/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,43 @@
+package hello.servlet_repeat.web.frontcontroller.v5.adapter;
+
+import hello.servlet_repeat.web.frontcontroller.ModelView;
+import hello.servlet_repeat.web.frontcontroller.v3.ControllerV3;
+import hello.servlet_repeat.web.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.jsp.PageContext;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.taglibs.standard.lang.jstl.ImplicitObjects.createParamMap;
+
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        ControllerV3 controller = (ControllerV3) handler;
+
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        return mv;
+
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName,
+                        request.getParameter(paramName)));
+        return paramMap;
+    }
+
+}
